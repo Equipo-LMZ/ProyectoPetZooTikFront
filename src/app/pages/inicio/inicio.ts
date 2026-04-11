87df27d2bb5fd05844a29a4e61cbc592ff5fdfcf
HarimDuenas <harimenriqueduenas@outlook.com>
2026-04-11 04:26:59 -0600

    Implementacion Incial del Parallax para la página de inicio, ademas de algunas animaciones y assets para este mismo

--- src/app/pages/inicio/inicio.ts
+++ src/app/pages/inicio/inicio.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
 import { ModalLogin } from '../../components/modal-login/modal-login';
 import { ModalRegistro } from '../../components/modal-registro/modal-registro';
 import { ModalCandidatura } from '../../components/modal-candidatura/modal-candidatura';
 
 @Component({
   selector: 'app-inicio',
-  imports: [ModalLogin, ModalRegistro, ModalCandidatura],
   standalone: true,
+  imports: [CommonModule, ModalLogin, ModalRegistro, ModalCandidatura],
   templateUrl: './inicio.html',
   styleUrl: './inicio.css',
 })
-export class Inicio {}
+export class Inicio {
+  escenaActual = signal<number>(1);
+  mostrarHelicoptero = signal<boolean>(true);
+
+  faseAnimacion = signal<number>(0);
+
+  constructor(private router: Router) {}
+
+  avanzarEscena() {
+    if (this.faseAnimacion() !== 0 || this.escenaActual() !== 1) return;
+
+    this.faseAnimacion.set(1);
+    setTimeout(() => this.mostrarHelicoptero.set(false), 200);
+
+    setTimeout(() => {
+      this.faseAnimacion.set(2);
+      
+      setTimeout(() => {
+        this.escenaActual.set(2);
+      }, 600); 
+
+    }, 300);
+  }
+
+  irAlMapa() {
+    this.router.navigate(['/mapa']);
+  }
+
+  entrarComoInvitado() {
+    this.router.navigate(['/mascotas']);
+  }
+}
