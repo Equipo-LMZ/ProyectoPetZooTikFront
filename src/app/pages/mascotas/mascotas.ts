3eda701b6af4ea220aa7f5d668c35ff3e65afc75
Julian Emmanuel Hernandez <julianemanuelhernandez@gmail.com>
2026-04-10 21:30:15 -0600

    Se estilizó todo el proceso que conlleva la adopción de los animales, se hizo con temática de un periodico, faltan las animaciones de despliegue y lo que se va a ver detras del periodico

--- src/app/pages/mascotas/mascotas.ts
+++ src/app/pages/mascotas/mascotas.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Animal } from '../../interfaces/animal';
 
 @Component({
   selector: 'app-mascotas',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
   templateUrl: './mascotas.html',
   styleUrl: './mascotas.css',
 })
-export class Mascotas {}
+export class Mascotas {
+  listaAnimales: Animal[] = [
+    {
+      id: 1,
+      nombre: 'Mantequilla',
+      descripcion:
+        'Un gato naranja muy perezoso que ama dormir al sol. Es experto en cazar moscas imaginarias.',
+      imagen: 'assets/pixel-cat.png',
+      tipoAnimal: 'Gato',
+      lugarRescate: 'Granja Pelican',
+      ubicacionActual: 'Refugio Central',
+    },
+    // ... más animalitos
+  ];
+
+  abrirModalAdopcion(animal: Animal) {
+    console.log('Abriendo formulario de adopción para:', animal.nombre);
+    // Aquí dispararías el modal que hicimos anteriormente
+  }
+}
