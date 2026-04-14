8bb0bcb074320491d0a90ba73f84870fefa01fd4
HarimDuenas <harimenriqueduenas@outlook.com>
2026-04-14 11:24:29 -0600

    Conexion del CRUD con el Back, falta corregir unos detalles

--- src/app/services/animal.ts
+++ src/app/services/animal.ts
@@ -2,45 +2,83 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
 import { Animal } from '../interfaces/animal';
+import { AuthService } from './auth';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AnimalService {
   private http = inject(HttpClient);
+  private authService = inject(AuthService);
   private baseUrl = 'https://api.petzootik.site';
 
   public listaAnimales = signal<Animal[]>([]);
 
-  // Obtiene un token fresco haciendo login
-  // esto hay q eliminarlo, es sólo de prueba
-  private async getToken(): Promise<string> {
-    const res = await lastValueFrom(
-      this.http.post<{ token: string }>(`${this.baseUrl}/user/login`, {
-        correo: 'JulianJoto@correo.com',
-        contrasena: 'julianJoto123',
-      }),
-    );
-    return res.token;
+  // Función para obtener el token en las peticiones CRUD es para facilitar la logica
+  private getHeaders() {
+    const token = this.authService.currentUser()?.token;
+    return new HttpHeaders({Authorization: `Bearer ${token}`})
   }
 
+  // Fetch (Ya se que no se llaman fetch) publicas
   async obtenerAnimales() {
     try {
-      const token = await this.getToken();
-      const headers = new HttpHeaders({ Authorization: `Bearer ${token}` });
       const data = await lastValueFrom(
-        this.http.get<Animal[]>(`${this.baseUrl}/pets`, { headers }),
+        this.http.get<Animal[]>(`${this.baseUrl}/pets`),
       );
       this.listaAnimales.set(data);
+      return data;
     } catch (error) {
-      console.error('Error al conectar con la lista de animales:', error);
+      console.error('Error al obtener los animales:', error);
       throw error;
     }
   }
 
   async obtenerPorId(id: number) {
-    const token = await this.getToken();
-    const headers = new HttpHeaders({ Authorization: `Bearer ${token}` });
-    return await lastValueFrom(this.http.get<Animal>(`${this.baseUrl}/pets/${id}`, { headers }));
+    try {
+        const data = await lastValueFrom(this.http.get<Animal>(`${this.baseUrl}/pets/${id}`),
+      );
+      return data;
+    } catch (error) {
+      console.error('Error al obtener el animal:', error);
+      throw error;
+    }
+  }
+
+  // Fetch (Ya se que no se llaman fetch) privadas
+  // CREATE
+  async crearAnimal(formData: FormData) {
+    try {
+      return await lastValueFrom(
+        this.http.post(`${this.baseUrl}/pets`, formData, { headers: this.getHeaders() })
+      );
+    } catch (error) {
+      console.error('Error al crear animal:', error);
+      throw error;
+    }
+  }
+
+  // UPDATE
+  async actualizarAnimal(id: number, formData: FormData) {
+    try {
+      return await lastValueFrom(
+        this.http.put(`${this.baseUrl}/pets/${id}`, formData, { headers: this.getHeaders() })
+      );
+    } catch (error) {
+      console.error('Error al actualizar animal:', error);
+      throw error;
+    }
+  }
+
+  // DELETE
+  async eliminarAnimal(id: number) {
+    try {
+      return await lastValueFrom(
+        this.http.delete(`${this.baseUrl}/pets/${id}`, { headers: this.getHeaders() })
+      );
+    } catch (error) {
+      console.error('Error al eliminar animal:', error);
+      throw error;
+    }
   }
 }
