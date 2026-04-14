faa3f82688067a65403a25e0b1986af87cb9eb6b
Julian Emmanuel Hernandez <julianemanuelhernandez@gmail.com>
2026-04-13 23:30:35 -0600

    se quitó el modal de prueba que estaba en 404

--- src/app/pages/not-found-404/not-found-404.ts
+++ src/app/pages/not-found-404/not-found-404.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { ModalCandidatura } from '../../components/modal-candidatura/modal-candidatura';
 
 @Component({
   selector: 'app-not-found-404',
-  imports: [ModalCandidatura],
+  imports: [],
   standalone: true,
   templateUrl: './not-found-404.html',
   styleUrl: './not-found-404.css',
