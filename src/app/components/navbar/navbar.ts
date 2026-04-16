16e93247099afe645a26ac6d758e7f9a4c7ea70d
Julian Emmanuel Hernandez <julianemanuelhernandez@gmail.com>
2026-04-16 10:26:25 -0600

    Implementación del navbar estático, faltan validaciones de Inicio de sesión y control del panel para que sólo salga si es rescatista

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { AuthService } from '../../services/auth';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ModalLogin } from '../modal-login/modal-login';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, ModalLogin],
   standalone: true,
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  public authService = inject(AuthService);
+  private router = inject(Router);
+
+  // Control del modal
+  verModal = signal<boolean>(false);
+
+  abrirLogin() {
+    this.verModal.set(true);
+  }
+
+  cerrarLogin() {
+    this.verModal.set(false);
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/']);
+  }
+}
