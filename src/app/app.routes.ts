91e160456553b8c16538a1ef67af987f5c050c6a
Santos <emiliano.santos_@outlook.com>
2026-04-18 15:12:12 -0600

    Se implementó la seguridad que evita que cualquier usuario pueda entrar al panel mediante la ruta.
    Se agregó un guard al código que verifica que el usuario este logueado y sea un rescatista. En caso de no serlo, manda un alert explicando el error y redirige al usuario al inicio. En las rutas de la aplicación se agregó este guard a todas las rutas relacionadas con el panel.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { rescatistaGuard } from './guards/rescatista.guard';
 
 export const routes: Routes = [
   // Rutas Públicas
@@ -26,6 +27,7 @@ export const routes: Routes = [
   // Rutas Privadas (Panel del Rescatista)
   {
     path: 'panel',
+    canActivate: [rescatistaGuard],
     loadComponent: () => import('./pages/panel-rescatista/panel-rescatista').then(m => m.PanelRescatista),
     children: [
       { 
